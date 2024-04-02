2fd8ed148723c0338109d61e307561c6561e0467
jiale <jialechen1909@gmail.com>
2024-04-02 13:34:21 +0200

    add MHA choice on test.py

--- scripts/test.py
+++ scripts/test.py
@@ -53,6 +53,7 @@ def parse_args():
     parser.add_argument('--encoder_size', type = int, default = 64)
     parser.add_argument('--if_write', type = bool, default = True)
     parser.add_argument('--pe', type = str, default = 'hybrid_sine')
+    parser.add_argument('--MHA', type = int, default = 1)
     opt = parser.parse_args()
     return opt
 
@@ -136,7 +137,7 @@ def tensors_to_csv(tensor1, tensor2, output_dir, cell_id, reshape_dim = (143, 5)
     plt.close()
 
 def train_model(train_dataloader, test_dataloader, decoder, optimizer, scheduler, criterion, epochs, batch_size, train_size, test_size, learning_rate,
-                num_layers, heads, dim_k, dim_v, dropout, encoder_size, device, if_write, pe, sequence_length = 6, predict_length = 1):
+                num_layers, heads, dim_k, dim_v, dropout, encoder_size, device, if_write, pe, MHA, sequence_length = 6, predict_length = 1):
     wandb.init(
         project = "Transformer_CDR",
         config = {
@@ -158,7 +159,8 @@ def train_model(train_dataloader, test_dataloader, decoder, optimizer, scheduler
             "predict_length": predict_length,
             "encoder_size": encoder_size,
             "criterion": "MSELoss",
-            "PE": pe}
+            "PE": pe,
+            "MHA": MHA}
     )
     wandb.watch(decoder)
     step = 0
@@ -178,7 +180,7 @@ def train_model(train_dataloader, test_dataloader, decoder, optimizer, scheduler
             input_mask = future_mask(input.shape[1]).unsqueeze(0).to(device)
 
             optimizer.zero_grad()
-            out = decoder(batch_size = num_cells, x = input, future_mask = input_mask, pe = pe, status = 'train')
+            out = decoder(batch_size = num_cells, x = input, future_mask = input_mask, pe = pe, MHA = MHA, status = 'train')
             out_loss = out[:, :-1, :]
             label_loss = input[:, 1:, :]
 
@@ -209,7 +211,7 @@ def train_model(train_dataloader, test_dataloader, decoder, optimizer, scheduler
         test_num_cells = len (cell_idx)
         for id, cell in enumerate(cell_idx):
             input = x_batch[id].unsqueeze(0).to(device)
-            out = decoder(batch_size = 1, x = input, future_mask = input_mask, pe = pe, status = 'predict')
+            out = decoder(batch_size = 1, x = input, future_mask = input_mask, pe = pe, MHA = MHA, status = 'predict')
             out_loss = out[:, :-1, :]
             label_loss = label[id]
             if if_write:
@@ -217,7 +219,7 @@ def train_model(train_dataloader, test_dataloader, decoder, optimizer, scheduler
             loss = criterion(out_loss, label_loss)
 
             test_batch_loss += loss.item ()
-        test_batch_num += 1
+        test_batch_num += test_num_cells
         test_total_loss += test_batch_loss
 
             #wandb.log({'cell_id': cell, 'prediction': out, 'target': label})
@@ -245,6 +247,7 @@ def main():
     encoder_size = opt.encoder_size
     if_write = opt.if_write
     pe = opt.pe
+    MHA = opt.MHA
 
 
     decoder = transformer_test.TransformerDecoder(embed_size = 5 , encoding_size = encoder_size, heads = heads, dim_k = dim_k, dim_v = dim_v,
@@ -287,7 +290,7 @@ def main():
     train_model(train_loader, test_loader, decoder, optimizer, scheduler, criterion, epochs = epochs, batch_size = batch_size,
                     train_size = train_size_factor, test_size = test_size_factor, learning_rate = learning_rate,
                     num_layers = num_layers, heads = heads, dim_k = dim_k, dim_v = dim_v, dropout = dropout,
-                    encoder_size = encoder_size, device = device, if_write = if_write, pe = pe, sequence_length = sequence_length, predict_length = predict_length)
+                    encoder_size = encoder_size, device = device, if_write = if_write, pe = pe, MHA = MHA ,sequence_length = sequence_length, predict_length = predict_length)
 
 
     return
